f08a2bd371f5f5fad77e13f92ead4fd04cd4261b
nobayene <sehlohone@dnecs.co.za>
2026-02-06 23:50:04 +0200

    Includs the Footer component in the Layout function

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,12 +1,14 @@
 import Navbar from './Navbar';
+import Footer from './Footer';
 
 export default function Layout({ children }) {
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen bg-gray-50 flex flex-col">
       <Navbar />
-      <main className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+      <main className="flex-1 max-w-7xl mx-auto py-6 sm:px-6 lg:px-8 w-full">
         {children}
       </main>
+      <Footer />
     </div>
   );
 }
