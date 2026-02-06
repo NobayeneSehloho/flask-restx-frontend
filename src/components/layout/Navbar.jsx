fdd0996face22747ed3201ba077542ea11ecd7f0
nobayene <sehlohone@dnecs.co.za>
2026-02-06 23:48:29 +0200

    Added some more nav links

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -37,6 +37,21 @@ export default function Navbar() {
               >
                 Students
               </Link>
+
+              <Link
+                to="/students"
+                className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+              >
+                Teachers
+              </Link>
+
+              <Link
+                to="/students"
+                className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+              >
+                Enrolment
+              </Link>
+
             </div>
           </div>
           <div className="flex items-center">
